dd5459da445e3d5908a0bb8332758cdc4b652c7e
mkemalarda <101436801+mkemalarda@users.noreply.github.com>
2023-09-28 15:17:47 +0300

    Add files via upload

---
+++ SpotifyApp/SpotifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAppApp.swift
+//  SpotifyApp
+//
+//  Created by Mustafa Kemal ARDA on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
